3e335e4efcbb8fc72d5c8a8da53404bb4b8566c7
Lenwee <lewishammond97@gmail.com>
2020-09-28 19:52:43 +0100

    package restructure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="sudoku-py",
+    name="sudoku_py",
     version="0.0.1",
     author="Lewis Hammond",
     author_email="lewishammond97@gmail.com",
