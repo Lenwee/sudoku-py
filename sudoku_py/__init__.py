9c058cc6b2f8051dfe78d002816e084e3e2643c8
Lenwee <lewishammond97@gmail.com>
2020-10-01 21:19:56 +0100

    Adds new generator for Sudoku changes

--- sudoku_py/__init__.py
+++ sudoku_py/__init__.py
@@ -1,1 +1,1 @@
-from .sudoku import Sudoku # NOQA
+from .sudoku import Sudoku, SudokuGenerator, SudokuException, CHAR_TOKENS, NUMBER_TOKENS  # NOQA
--- sudoku_py/sudoku.py
+++ sudoku_py/sudoku.py
@@ -1,5 +1,6 @@
 import copy
 import math
+import random
 import string
 
 NUMBER_TOKENS = 'N'
@@ -19,6 +20,7 @@ class Sudoku:
 
     def __init__(self, board, block_width=None, block_height=None, board_token=NUMBER_TOKENS):
         self.board = board
+        self.board_width = len(board)
         if not block_height and not block_width:
             self.block_height, self.block_width = self.__get_block_dimensions()
         elif not block_height:
@@ -37,12 +39,12 @@ class Sudoku:
 
     def get_board_tokens(self):
         if self.board_token is NUMBER_TOKENS:
-            return list(range(1, len(self.board)+1))
+            return list(range(1, self.board_width + 1))
         elif self.board_token is CHAR_TOKENS:
-            return list(string.ascii_lowercase[:len(self.board)])
+            return list(string.ascii_lowercase[:self.board_width])
 
     def board_is_valid(self):
-        board_height = len(self.board)
+        board_height = self.board_width
         for row in self.board:
             if len(row) != board_height:
                 raise SudokuException('Board dimensions are invalid.')
@@ -59,7 +61,7 @@ class Sudoku:
         self.__solve(solutions=solutions)
 
     def __get_block_dimensions(self):
-        block_area = len(self.board)
+        block_area = self.board_width
         height = 0
         width = math.ceil(math.sqrt(block_area))
         while width <= block_area:
@@ -69,7 +71,7 @@ class Sudoku:
             width += 1
         return height, width
 
-    def __possible(self, x, y, value):
+    def possible(self, x, y, value):
         return (self.__check_row(y, value) and
                 self.__check_col(x, value) and
                 self.__check_block(x, y, value))
@@ -85,23 +87,23 @@ class Sudoku:
         return True
 
     def __check_row(self, y, value):
-        for x in range(len(self.board)):
+        for x in range(self.board_width):
             if self.board[y][x] == value:
                 return False
         return True
 
     def __check_col(self, x, value):
-        for y in range(len(self.board)):
+        for y in range(self.board_width):
             if self.board[y][x] == value:
                 return False
         return True
 
     def __solve(self, solutions):
-        for y in range(len(self.board)):
-            for x in range(len(self.board)):
+        for y in range(self.board_width):
+            for x in range(self.board_width):
                 if self.board[y][x] == 0:
                     for value in self.board_tokens:
-                        if self.__possible(x, y, value):
+                        if self.possible(x, y, value):
                             self.board[y][x] = value
                             self.__solve(solutions=solutions)
                             if len(self.solutions) >= solutions:
@@ -109,3 +111,71 @@ class Sudoku:
                             self.board[y][x] = 0
                     return
         self.solutions.append(copy.deepcopy(self.board))
+
+
+class SudokuGenerator(Sudoku):
+
+    def __init__(self, board_width, block_width=None, block_height=None):
+        self.board_width = board_width
+        super(SudokuGenerator, self).__init__(
+            board=self.__create_empty_board(),
+            block_width=block_width,
+            block_height=block_height,
+            board_token=CHAR_TOKENS
+        )
+        self.__board_created = False
+
+    @staticmethod
+    def __get_random_value(cell_options):
+        return random.choice(cell_options)
+
+    def __create_empty_board(self):
+        board = []
+        for y in range(self.board_width):
+            board.append([])
+            for x in range(self.board_width):
+                board[y].append(0)
+        return board
+
+    def __create_full_grid(self):
+        for y in range(self.board_width):
+            for x in range(self.board_width):
+                if self.board[y][x] == 0:
+                    cell_options = copy.deepcopy(self.board_tokens)
+                    while len(cell_options) > 0:
+                        if len(cell_options) == 0:
+                            break
+                        value = self.__get_random_value(cell_options)
+                        if self.possible(x, y, value):
+                            self.board[y][x] = value
+                            self.__create_full_grid()
+                            if self.__board_created:
+                                return
+                            self.board[y][x] = 0
+                        cell_options.remove(value)
+                    return
+        self.__board_created = True
+        return
+
+    def generate(self, cells_to_remove):
+        self.__create_full_grid()
+        grid_coords = []
+        for y in range(self.board_width):
+            for x in range(self.board_width):
+                grid_coords.append((x, y))
+        random.shuffle(grid_coords)
+        for x, y in grid_coords:
+            if cells_to_remove == 0:
+                return self.board
+
+            if self.board[y][x] != 0:
+                prev_value = self.board[y][x]
+                self.board[y][x] = 0
+                temp_grid = copy.deepcopy(self.board)
+                self.solve(solutions=1)
+                self.board = temp_grid
+                cells_to_remove -= 1
+                if len(self.solutions) != 1:
+                    self.board[y][x] = prev_value
+                    cells_to_remove += 1
+        return self.board
